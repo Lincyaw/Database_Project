2fe7e88f9882b92a705efd2f41dbedb80549fdad
lincyaw <814750204@qq.com>
2021-04-22 14:55:18 +0800

    实验四起步

---
+++ extmem-c/cmake-build-debug/CMakeFiles/extmem_c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extmem_c.dir/extmem.c.obj"
+  "CMakeFiles/extmem_c.dir/test.c.obj"
+  "extmem_c.exe"
+  "extmem_c.exe.manifest"
+  "extmem_c.lib"
+  "extmem_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/extmem_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
